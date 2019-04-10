e94acb4dac7e5277325bfc238bda67470f5d7dee
1-n-only-nobody <36240714+1-n-only-nobody@users.noreply.github.com>
2019-04-10 22:59:49 +0530

    Swipe view added(not working yet)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.myapplication;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -10,6 +16,9 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    //private static final int NUM_PAGES = 4;
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -40,8 +49,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        pagerAdapter = new SwipePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+}
+class SwipePagerAdapter extends FragmentStatePagerAdapter implements HomeFragment.OnFragmentInteractionListener,VideoFragment.OnFragmentInteractionListener,DashboardFragment.OnFragmentInteractionListener,ProfileFragment.OnFragmentInteractionListener{
+
+    public SwipePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        switch (i){
+            case 0:
+                HomeFragment homeFragment = new HomeFragment();
+                return homeFragment;
+            case 1:
+                VideoFragment videoFragment = new VideoFragment();
+                return videoFragment;
+            case 2:
+                DashboardFragment dashboardFragment = new DashboardFragment();
+                return dashboardFragment;
+            case 3:
+                ProfileFragment profileFragment = new ProfileFragment();
+                return profileFragment;
+        }
+        return null;
+
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
 }
