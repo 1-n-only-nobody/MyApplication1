c77a4341412758e2edaad7d277dc423e8dd28def
Parth19499 <36495949+Parth19499@users.noreply.github.com>
2019-04-11 15:59:54 +0530

    Login Layout added. Still not working.

--- app/src/main/java/com/example/myapplication/apiTester.java
+++ app/src/main/java/com/example/myapplication/apiTester.java
@@ -39,7 +39,7 @@ public class apiTester extends AppCompatActivity {
                 JSONObject json = new JSONObject(response);
                 tv.setText(json.toString());
             } catch (JSONException e) {
-                Toast.makeText(apiTester.this, "error", Toast.LENGTH_SHORT).show();
+                Toast.makeText(apiTester.this, "error", Toast.LENGTH_LONG).show();
                 e.printStackTrace();
             }
 
